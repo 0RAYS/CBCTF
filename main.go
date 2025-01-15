047f573579129a3956288b94c9e751bdb64935a8
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-01-15 12:21:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"CBCTF/internal/config"
 	"CBCTF/internal/db"
+	"CBCTF/internal/i18n"
 	"CBCTF/internal/log"
+	"CBCTF/internal/router"
+	"fmt"
 )
 
 func init() {
+	i18n.Init()
 	config.Init()
 	log.Init()
 	db.Init()
 }
 
 func main() {
+	ip, port := config.Env.GetString("gin.ip"), config.Env.GetString("gin.port")
+	if err := router.Init().Run(fmt.Sprintf("%s:%s", ip, port)); err != nil {
+		log.Logger.Panicf("Failed to start: %s", err)
+	}
 }
