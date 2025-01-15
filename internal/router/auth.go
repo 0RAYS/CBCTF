21cd71f0d3eb294bf19a37644e2dc83c87be83de
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-01-15 11:35:53 +0800

    Create auth handler and do test

---
+++ internal/router/auth.go
@@ -0,0 +1,97 @@
+package router
+
+import (
+	"CBCTF/internal/db"
+	"CBCTF/internal/log"
+	"CBCTF/internal/middleware"
+	"CBCTF/internal/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Register(ctx *gin.Context) {
+	var registerForm RegisterForm
+	trace := middleware.GetTraceID(ctx)
+	if err := ctx.ShouldBind(&registerForm); err == nil {
+		username, password, email := registerForm.Name, registerForm.Password, registerForm.Email
+		user, ok, msg := db.CreateUser(ctx, username, password, email)
+		if !ok {
+			log.Logger.Infof("%s | %s", trace, msg)
+			ctx.JSONP(http.StatusUnauthorized, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": nil})
+			return
+		}
+		if Token, err := utils.Generate(user.ID, user.Name, "user"); err == nil {
+			log.Logger.Infof("%s | %s:%d register", trace, user.Name, user.ID)
+			ctx.Writer.Header().Set("Authorization", "Bearer "+Token)
+			ctx.JSONP(http.StatusOK, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": user})
+			return
+		} else {
+			msg = "UnknownError"
+			log.Logger.Errorf("%s | Generate token error: %s", trace, err)
+			ctx.JSONP(http.StatusInternalServerError, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": nil})
+			return
+		}
+	} else {
+		log.Logger.Infof("%s | %s", trace, err)
+		ctx.JSONP(http.StatusBadRequest, gin.H{"trace": trace, "msg": utils.M(ctx, "BadRequest"), "data": nil})
+		return
+	}
+}
+
+func Login(ctx *gin.Context) {
+	var loginForm LoginForm
+	trace := middleware.GetTraceID(ctx)
+	if err := ctx.ShouldBind(&loginForm); err == nil {
+		username, password := loginForm.Name, loginForm.Password
+		user, ok, msg := db.VerifyUser(ctx, username, password)
+		if !ok {
+			log.Logger.Infof("%s | %s", trace, msg)
+			ctx.JSONP(http.StatusUnauthorized, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": nil})
+			return
+		}
+		if Token, err := utils.Generate(user.ID, user.Name, "user"); err == nil {
+			log.Logger.Infof("%s | %s:%d login", trace, user.Name, user.ID)
+			ctx.Writer.Header().Set("Authorization", "Bearer "+Token)
+			ctx.JSONP(http.StatusOK, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": user})
+			return
+		} else {
+			msg = "UnknownError"
+			log.Logger.Errorf("%s | Generate token error: %s", trace, err)
+			ctx.JSONP(http.StatusInternalServerError, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": nil})
+			return
+		}
+	} else {
+		log.Logger.Infof("%s | %s", trace, err)
+		ctx.JSONP(http.StatusBadRequest, gin.H{"trace": trace, "msg": utils.M(ctx, "BadRequest"), "data": nil})
+		return
+	}
+}
+
+func AdminLogin(ctx *gin.Context) {
+	var loginForm LoginForm
+	trace := middleware.GetTraceID(ctx)
+	if err := ctx.ShouldBind(&loginForm); err == nil {
+		username, password := loginForm.Name, loginForm.Password
+		admin, ok, msg := db.VerifyAdmin(ctx, username, password)
+		if !ok {
+			log.Logger.Infof("%s | %s", trace, msg)
+			ctx.JSONP(http.StatusUnauthorized, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": nil})
+			return
+		}
+		if Token, err := utils.Generate(admin.ID, admin.Name, "admin"); err == nil {
+			log.Logger.Infof("%s | %s:%d login", trace, admin.Name, admin.ID)
+			ctx.Writer.Header().Set("Authorization", "Bearer "+Token)
+			ctx.JSONP(http.StatusOK, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": admin})
+			return
+		} else {
+			msg = "UnknownError"
+			log.Logger.Errorf("%s | Generate token error: %s", trace, err)
+			ctx.JSONP(http.StatusInternalServerError, gin.H{"trace": trace, "msg": utils.M(ctx, msg), "data": nil})
+			return
+		}
+	} else {
+		log.Logger.Infof("%s | %s", trace, err)
+		ctx.JSONP(http.StatusBadRequest, gin.H{"trace": trace, "msg": utils.M(ctx, "BadRequest"), "data": nil})
+		return
+	}
+}
---
+++ internal/router/auth_test.go
@@ -0,0 +1,135 @@
+package router
+
+import (
+	"CBCTF/internal/config"
+	"CBCTF/internal/db"
+	"CBCTF/internal/i18n"
+	"CBCTF/internal/log"
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func InitAuthTest() *gin.Engine {
+	i18n.Init()
+	config.Env = viper.New()
+	config.Env.Set("gorm.file", "test.db")
+	config.Env.Set("gorm.log.level", "silent")
+	config.Env.Set("log.level", "info")
+	config.Env.Set("log.file", false)
+	config.Env.Set("gin.mode", "release")
+	log.Init()
+	db.Init()
+	var ctx context.Context
+	db.CreateUser(ctx, "user1", "password", "user1@0rays.club")
+	db.CreateAdmin(ctx, "admin1", "password", "admin1@0rays.club")
+	return Init()
+}
+
+func TestRegister(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := InitAuthTest()
+
+	w := httptest.NewRecorder()
+	body := `{"name":"user2","password":"password","email":"user2@0rays.club"}`
+	req, _ := http.NewRequest("POST", "/register", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept-Language", "")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, w.Code)
+	}
+
+	w = httptest.NewRecorder()
+	body = `{"name":"user1","password":"password","email":"user1@0rays.club"}`
+	req, _ = http.NewRequest("POST", "/register", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept-Language", "")
+
+	router.ServeHTTP(w, req)
+	if w.Code != http.StatusUnauthorized {
+		t.Errorf("Expected status %d but got %d", http.StatusInternalServerError, w.Code)
+	}
+}
+
+func TestLogin(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := InitAuthTest()
+
+	w := httptest.NewRecorder()
+	body := `{"name":"user1","password":"password"}`
+	req, _ := http.NewRequest("POST", "/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, w.Code)
+	}
+
+	w = httptest.NewRecorder()
+	body = `{"name":"user1@0rays.club","password":"password"}`
+	req, _ = http.NewRequest("POST", "/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, w.Code)
+	}
+
+	w = httptest.NewRecorder()
+	body = `{"name":"user1","password":"error_pwd"}`
+	req, _ = http.NewRequest("POST", "/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusUnauthorized {
+		t.Errorf("Expected status %d but got %d", http.StatusUnauthorized, w.Code)
+	}
+}
+
+func TestAdminLogin(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := InitAuthTest()
+
+	w := httptest.NewRecorder()
+	body := `{"name":"admin1","password":"password"}`
+	req, _ := http.NewRequest("POST", "/admin/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, w.Code)
+	}
+
+	w = httptest.NewRecorder()
+	body = `{"name":"admin1@0rays.club","password":"password"}`
+	req, _ = http.NewRequest("POST", "/admin/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status %d but got %d", http.StatusOK, w.Code)
+	}
+
+	w = httptest.NewRecorder()
+	body = `{"name":"admin1","password":"error_pwd"}`
+	req, _ = http.NewRequest("POST", "/admin/login", strings.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+
+	if w.Code != http.StatusUnauthorized {
+		t.Errorf("Expected status %d but got %d", http.StatusUnauthorized, w.Code)
+	}
+}
