1c154e6ab28c12f1dd145048b72c21cd02c69787
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-02-06 17:28:00 +0800

    将关联模式替换为直接操作关联模型

--- internal/db/association.go
+++ internal/db/association.go
@@ -7,30 +7,20 @@ import (
 
 // AppendUserToTeam Many2Many
 func AppendUserToTeam(tx *gorm.DB, user model.User, team model.Team) error {
-	return tx.Model(&team).Association("Users").Append(&user)
+	return tx.Model(&model.UserTeam{}).Create(&model.UserTeam{UserID: user.ID, TeamID: team.ID}).Error
 }
 
 // AppendUserToContest Many2Many
 func AppendUserToContest(tx *gorm.DB, user model.User, contest model.Contest) error {
-	return tx.Model(&contest).Association("Users").Append(&user)
-}
-
-// AppendTeamToContest HasMany
-func AppendTeamToContest(tx *gorm.DB, team model.Team, contest model.Contest) error {
-	return tx.Model(&contest).Association("Teams").Append(&team)
+	return tx.Model(&model.UserContest{}).Create(&model.UserContest{UserID: user.ID, ContestID: contest.ID}).Error
 }
 
 // DeleteUserFromTeam Many2Many
 func DeleteUserFromTeam(tx *gorm.DB, user model.User, team model.Team) error {
-	return tx.Model(&team).Association("Users").Delete(&user)
+	return tx.Model(&model.UserTeam{}).Delete(&model.UserTeam{UserID: user.ID, TeamID: team.ID}).Error
 }
 
 // DeleteUserFromContest Many2Many
 func DeleteUserFromContest(tx *gorm.DB, user model.User, contest model.Contest) error {
-	return tx.Model(&contest).Association("Users").Delete(&user)
-}
-
-// DeleteTeamFromContest HasMany
-func DeleteTeamFromContest(tx *gorm.DB, team model.Team, contest model.Contest) error {
-	return tx.Model(&contest).Association("Teams").Delete(&team)
+	return tx.Model(&model.UserContest{}).Delete(&model.UserContest{UserID: user.ID, ContestID: contest.ID}).Error
 }
