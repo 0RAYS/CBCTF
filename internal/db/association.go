d9125b74cb9760fbf588f5d647752a2f6e8645fd
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-02-11 15:47:31 +0800

    Fixed bug

--- internal/db/association.go
+++ internal/db/association.go
@@ -17,10 +17,12 @@ func AppendUserToContest(tx *gorm.DB, user model.User, contest model.Contest) er
 
 // DeleteUserFromTeam Many2Many
 func DeleteUserFromTeam(tx *gorm.DB, user model.User, team model.Team) error {
-	return tx.Model(&model.UserTeam{}).Delete(&model.UserTeam{UserID: user.ID, TeamID: team.ID}).Error
+	return tx.Model(&model.UserTeam{}).Where("user_id = ? AND team_id = ?", user.ID, team.ID).
+		Delete(&model.UserTeam{}).Error
 }
 
 // DeleteUserFromContest Many2Many
 func DeleteUserFromContest(tx *gorm.DB, user model.User, contest model.Contest) error {
-	return tx.Model(&model.UserContest{}).Delete(&model.UserContest{UserID: user.ID, ContestID: contest.ID}).Error
+	return tx.Model(&model.UserContest{}).Where("user_id = ? AND contest_id = ?", user.ID, contest.ID).
+		Delete(&model.UserContest{}).Error
 }
