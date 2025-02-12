1287dba3b9a1b084c774f28ae0a5bbea3a19e431
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-02-12 23:13:11 +0800

    Fixed bug

--- internal/db/device.go
+++ internal/db/device.go
@@ -3,13 +3,13 @@ package db
 import (
 	"CBCTF/internal/log"
 	"CBCTF/internal/model"
-	"errors"
 	"gorm.io/gorm"
+	"strings"
 )
 
 // RecordDevice 记录设备 ID
 func RecordDevice(tx *gorm.DB, device model.Device) {
-	if err := tx.Model(&model.Device{}).Create(&device).Error; err != nil && !errors.Is(err, gorm.ErrDuplicatedKey) {
+	if err := tx.Model(&model.Device{}).Create(&device).Error; err != nil && !strings.Contains(err.Error(), "Error 1062") {
 		log.Logger.Warningf("Failed to record device %d-%s: %s", device.UserID, device.Magic, err)
 	}
 }
