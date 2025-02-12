03550c17a4bcc7820f0f7553c891c2996631e35d
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-02-12 23:08:25 +0800

    Fixed bug

--- internal/db/device.go
+++ internal/db/device.go
@@ -9,7 +9,7 @@ import (
 
 // RecordDevice 记录设备 ID
 func RecordDevice(tx *gorm.DB, device model.Device) {
-	if err := tx.Model(&model.IP{}).Create(&device).Error; err != nil && !errors.Is(err, gorm.ErrDuplicatedKey) {
+	if err := tx.Model(&model.Device{}).Create(&device).Error; err != nil && !errors.Is(err, gorm.ErrDuplicatedKey) {
 		log.Logger.Warningf("Failed to record IP %d-%s", device.UserID, device.Magic)
 	}
 }
