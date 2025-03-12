1df8f1fc029844c22607d70302c049632974ab67
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-12 19:45:04 +0800

    Update magic.go

--- internal/middleware/magic.go
+++ internal/middleware/magic.go
@@ -8,6 +8,11 @@ import (
 
 func SetMagic(ctx *gin.Context) {
 	magic := ctx.GetHeader("X-M")
+	if strings.HasPrefix(ctx.Request.RequestURI, "/admin/") {
+		ctx.Set("Magic", magic)
+		ctx.Next()
+		return
+	}
 	if !(strings.HasPrefix(ctx.Request.RequestURI, "/avatars/") && len(ctx.Request.RequestURI) == 45) && magic == "" {
 		ctx.JSON(http.StatusBadRequest, gin.H{"msg": "BadRequest", "data": nil})
 		ctx.Abort()
