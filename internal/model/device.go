3567dba713f18c56a912c1edd31c46223cadf1d7
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-14 14:46:44 +0800

    Update device.go

--- internal/model/device.go
+++ internal/model/device.go
@@ -1,6 +1,7 @@
 package model
 
 type Device struct {
+	ID     uint   `gorm:"primaryKey" json:"id"`
 	UserID uint   `gorm:"index:idx_user_id_magic,unique;" json:"user_id"`
 	Magic  string `gorm:"index:idx_user_id_magic,unique;" json:"magic"`
 	Count  int    `json:"count"`
