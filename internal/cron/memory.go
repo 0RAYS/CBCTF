07301bbfd230dd1f7bc431ea43dea6001c5dca1f
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-14 20:58:42 +0800

    优化contest获取逻辑

--- internal/cron/memory.go
+++ internal/cron/memory.go
@@ -2,6 +2,7 @@ package cron
 
 import (
 	"CBCTF/internal/db"
+	"CBCTF/internal/model"
 	"github.com/robfig/cron/v3"
 	"time"
 )
@@ -9,17 +10,21 @@ import (
 // ClearUsageMutex 定时任务清理flag提交锁 db.UsagesMutex
 func ClearUsageMutex(c *cron.Cron) {
 	function := func() {
+		var contests map[uint]model.Contest
 		db.UsagesMutex.Range(func(k, v interface{}) bool {
 			usage, ok, _ := db.GetUsageByID(db.DB, k.(uint))
 			if !ok {
 				return true
 			}
-			contest, ok, _ := db.GetContestByID(db.DB, usage.ContestID)
-			if !ok {
-				return true
-			}
-			if !contest.IsRunning() {
-				db.UsagesMutex.Delete(k)
+			if contest, ok := contests[usage.ContestID]; !ok {
+				contest, ok, _ = db.GetContestByID(db.DB, usage.ContestID)
+				if !ok {
+					return true
+				}
+				contests[usage.ContestID] = contest
+				if !contest.IsRunning() {
+					db.UsagesMutex.Delete(k)
+				}
 			}
 			return true
 		})
