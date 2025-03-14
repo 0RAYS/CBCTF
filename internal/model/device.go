70a30aae04ede148c2b6599c163c1444e664a414
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-14 14:47:17 +0800

    Update device.go

--- internal/model/device.go
+++ internal/model/device.go
@@ -1,8 +1,16 @@
 package model
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type Device struct {
-	ID     uint   `gorm:"primaryKey" json:"id"`
-	UserID uint   `gorm:"index:idx_user_id_magic,unique;" json:"user_id"`
-	Magic  string `gorm:"index:idx_user_id_magic,unique;" json:"magic"`
-	Count  int    `json:"count"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	UserID    uint           `gorm:"index:idx_user_id_magic,unique;" json:"user_id"`
+	Magic     string         `gorm:"index:idx_user_id_magic,unique;" json:"magic"`
+	Count     int            `json:"count"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
