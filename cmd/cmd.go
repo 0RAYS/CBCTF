0df3580343480daca2b79056b069cea317df8a0b
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-22 00:18:35 +0800

    Update cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -5,6 +5,7 @@ import (
 	"CBCTF/internal/k8s"
 	"CBCTF/internal/log"
 	"flag"
+	"os"
 )
 
 func init() {
@@ -13,29 +14,22 @@ func init() {
 }
 
 func Cmd() {
-	cmd := flag.NewFlagSet("k8s", flag.ExitOnError)
-	init := cmd.Bool("init", false, "init k8s")
-	check := cmd.Bool("check", false, "check k8s")
-
-	flag.Parse()
-	args := flag.Args()
-
-	switch len(args) {
-	case 0, 1:
+	if len(os.Args) < 3 {
 		run()
-	case 2:
-		err := cmd.Parse(args[1:])
-		if err != nil {
-			log.Logger.Fatalf("Failed to parse command: %v", err)
-		}
-		k8s.Init(false)
-		if *init {
-			k8s.InitResources()
-		} else if *check {
-			k8s.CheckPermission()
-		} else {
-			run()
-		}
+	}
+	cmd := flag.NewFlagSet("k8s", flag.ExitOnError)
+	err := cmd.Parse(os.Args[2:])
+	if err != nil {
+		log.Logger.Fatalf("Failed to parse command: %v", err)
+	}
 
+	k8s.Init(false)
+	switch os.Args[2] {
+	case "init":
+		k8s.InitResources()
+	case "check":
+		k8s.CheckPermission()
+	default:
+		run()
 	}
 }
