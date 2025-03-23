825df3555d5bde9b8f1d6db910b777436b3d466e
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-24 01:21:00 +0800

    移除

--- internal/model/challenge.go
+++ internal/model/challenge.go
@@ -21,20 +21,19 @@ const (
 )
 
 type Challenge struct {
-	ID             string `json:"id" gorm:"primaryKey"`
-	Name           string `json:"name" gorm:"not null"`
-	Desc           string `json:"desc"`
-	Flag           string `json:"flag"`
-	Category       string `json:"category"`
-	Type           string `json:"type" gorm:"default:'static'"`
-	GeneratorImage string `json:"generator" gorm:"column:generator"`
-	DockerImage    string `json:"docker" gorm:"column:docker"`
-	Port           int32  `json:"port" gorm:"default:8080"`
-	//Dockers        utils.Dockers          `json:"dockers" gorm:"type:json"`
-	CreatedAt time.Time              `json:"-"`
-	UpdatedAt time.Time              `json:"-"`
-	DeletedAt gorm.DeletedAt         `json:"-" gorm:"index"`
-	Version   optimisticlock.Version `json:"-" gorm:"default:1"`
+	ID             string                 `json:"id" gorm:"primaryKey"`
+	Name           string                 `json:"name" gorm:"not null"`
+	Desc           string                 `json:"desc"`
+	Flag           string                 `json:"flag"`
+	Category       string                 `json:"category"`
+	Type           string                 `json:"type" gorm:"default:'static'"`
+	GeneratorImage string                 `json:"generator" gorm:"column:generator"`
+	DockerImage    string                 `json:"docker" gorm:"column:docker"`
+	Port           int32                  `json:"port" gorm:"default:8080"`
+	CreatedAt      time.Time              `json:"-"`
+	UpdatedAt      time.Time              `json:"-"`
+	DeletedAt      gorm.DeletedAt         `json:"-" gorm:"index"`
+	Version        optimisticlock.Version `json:"-" gorm:"default:1"`
 }
 
 // BasicDir 获取题目相关文件的目录
