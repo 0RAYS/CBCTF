bd80c3ab13ef749d8a11e6bbbbb573b2a3f0b998
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-23 14:39:35 +0800

    重命名参数

--- internal/k8s/docker.go
+++ internal/k8s/docker.go
@@ -12,7 +12,7 @@ import (
 )
 
 // StartContainer 启动容器, 并且注入 flag
-func StartContainer(usage model.Usage, flag model.Flag, docker model.Container) (string, int32, bool, string) {
+func StartContainer(usage model.Usage, flag model.Flag, container model.Container) (string, int32, bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancel()
 	var (
@@ -29,18 +29,18 @@ func StartContainer(usage model.Usage, flag model.Flag, docker model.Container) 
 		return "", -1, false, "EmptyDockerImage"
 	}
 	log.Logger.Debugf("Creating Pod for challenge %s:%s", usage.Name, usage.ChallengeID)
-	service, ok, msg = CreateService(ctx, docker, usage)
+	service, ok, msg = CreateService(ctx, container, usage)
 	if !ok {
 		return "", -1, false, msg
 	}
-	_, ok, msg = CreateNetworkPolicy(ctx, docker, usage)
+	_, ok, msg = CreateNetworkPolicy(ctx, container, usage)
 	if !ok {
 		return "", -1, false, msg
 	}
 	port := service.Spec.Ports[0].NodePort
 	containers := []corev1.Container{
 		{
-			Name:  docker.ContainerName,
+			Name:  container.ContainerName,
 			Image: usage.DockerImage,
 			Env: []corev1.EnvVar{
 				{
@@ -81,7 +81,7 @@ func StartContainer(usage model.Usage, flag model.Flag, docker model.Container) 
 				},
 				{
 					Name:  "name",
-					Value: docker.PodName,
+					Value: container.PodName,
 				},
 				{
 					Name:  "type",
@@ -103,9 +103,9 @@ func StartContainer(usage model.Usage, flag model.Flag, docker model.Container) 
 		}
 		containers = append(containers, frpc)
 		ip = frps.Host
-		log.Logger.Infof("Frpc started: %s:%d -> %s:%d", frps.Host, port, docker.PodName, port)
+		log.Logger.Infof("Frpc started: %s:%d -> %s:%d", frps.Host, port, container.PodName, port)
 	}
-	pod, ok, msg = CreatePod(ctx, docker, usage, containers)
+	pod, ok, msg = CreatePod(ctx, usage, container.PodName, containers)
 	if !ok {
 		return "", -1, false, msg
 	}
@@ -117,22 +117,22 @@ func StartContainer(usage model.Usage, flag model.Flag, docker model.Container) 
 }
 
 // StopContainer 停止容器
-func StopContainer(docker model.Container) (bool, string) {
+func StopContainer(container model.Container) (bool, string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancel()
 	var err error
 	err = CopyFromPod(
-		docker.PodName, "tcpdump", "/root/traffic.pcap",
-		docker.TrafficPath(),
+		container.PodName, "tcpdump", "/root/traffic.pcap",
+		container.TrafficPath(),
 	)
 	if err != nil {
-		log.Logger.Warningf("Failed to copy %d traffic: %v", docker.TeamID, err)
+		log.Logger.Warningf("Failed to copy %d traffic: %v", container.TeamID, err)
 	}
-	if ok, msg := DeleteNetworkPolicy(ctx, docker.NetworkPolicyName); !ok {
+	if ok, msg := DeleteNetworkPolicy(ctx, container.NetworkPolicyName); !ok {
 		return false, msg
 	}
-	if ok, msg := DeleteService(ctx, docker.ServiceName); !ok {
+	if ok, msg := DeleteService(ctx, container.ServiceName); !ok {
 		return false, msg
 	}
-	return DeletePod(ctx, docker.PodName)
+	return DeletePod(ctx, container.PodName)
 }
