b87edd8748d064df446e8f78c4e76862ae8eb3be
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-26 19:59:36 +0800

    feat: 增加 计算 分数 和 血 的函数

--- internel/model/flag.go
+++ internel/model/flag.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"gorm.io/gorm"
+	"math"
 	"time"
 )
 
@@ -31,4 +32,40 @@ type Flag struct {
 	UpdatedAt    time.Time      `json:"-"`
 	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
 	Version      uint           `gorm:"default:1" json:"-"`
+}
+
+func (f *Flag) CalcNewScore(solvers int64) float64 {
+	var calc float64 = 0
+	switch f.ScoreType {
+	case StaticScore:
+		calc = f.Score
+	case LinearScore:
+		calc = f.Score - float64(solvers)*f.Decay
+	case LogarithmicScore:
+		calc = (f.MinScore-f.Score)/(f.Decay*f.Decay)*float64(solvers*solvers) + f.Score
+	default:
+		calc = f.Score
+	}
+	if calc < f.MinScore {
+		calc = f.MinScore
+	}
+	calc = math.Trunc(calc*100) / 100
+	return calc
+}
+
+func (f *Flag) CalcBlood(teamID uint) (float64, string) {
+	mapping := []struct {
+		value float64
+		name  string
+	}{
+		{0.05, "first"},
+		{0.03, "second"},
+		{0.01, "third"},
+	}
+	for i := 0; i < len(f.Blood) && i < 3; i++ {
+		if f.Blood[i] == 0 || f.Blood[i] == teamID {
+			return mapping[i].value, mapping[i].name
+		}
+	}
+	return 0, ""
 }
