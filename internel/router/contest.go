b417d872f1676fc811e0610610a17233b8cc29c2
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-26 20:20:01 +0800

    feat: 完善函数

--- internel/router/contest.go
+++ internel/router/contest.go
@@ -2,12 +2,20 @@ package router
 
 import (
 	"CBCTF/internel/middleware"
+	db "CBCTF/internel/repo"
 	"CBCTF/internel/resp"
+	"CBCTF/internel/service"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func GetContest(ctx *gin.Context) {
 	contest := middleware.GetContest(ctx)
-	ctx.JSON(http.StatusOK, gin.H{"msg": "Success", "data": resp.GetContestResp(contest)})
+	champion, _, _, _ := service.GetTeamRanking(db.DB.WithContext(ctx), contest.ID, 1, 0)
+	data := resp.GetContestResp(contest)
+	data["highest"] = 0
+	if len(champion) > 0 {
+		data["highest"] = champion[0].Score
+	}
+	ctx.JSON(http.StatusOK, gin.H{"msg": "Success", "data": data})
 }
