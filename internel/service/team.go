53feae8067505c90f32d8146d0c404fed9f95251
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-03-27 11:21:21 +0800

    chore: 移动函数

--- internel/service/team.go
+++ internel/service/team.go
@@ -3,7 +3,6 @@ package service
 import (
 	f "CBCTF/internel/form"
 	"CBCTF/internel/model"
-	"CBCTF/internel/redis"
 	db "CBCTF/internel/repo"
 	"CBCTF/internel/utils"
 	"gorm.io/gorm"
@@ -140,54 +139,6 @@ func LeaveTeam(tx *gorm.DB, contest model.Contest, team model.Team, userID uint)
 		return false, "DeleteUserFromContestError"
 	}
 	return true, "Success"
-}
-
-func UpdateTeamRanking(tx *gorm.DB, contestID uint) (bool, string) {
-	var (
-		repo              = db.InitTeamRepo(tx)
-		teams, _, ok, msg = repo.GetAll(contestID, -1, -1, true, 0, true, false)
-		score             float64
-		err               error
-	)
-	if !ok {
-		return false, msg
-	}
-	for _, team := range teams {
-		score, ok, msg = CalcTeamScore(tx, team.ID)
-		if !ok {
-			continue
-		}
-		// 不考虑更新失败的情况, 不回滚
-		repo.Update(team.ID, db.UpdateTeamOptions{Score: &score})
-	}
-	teams, _, ok, msg = repo.GetAll(contestID, -1, -1, true, 0, true, false)
-	if !ok {
-		return false, msg
-	}
-	if err = redis.UpdateTeamRanking(contestID, teams); err != nil {
-		return false, "UpdateRankingError"
-	}
-	return true, "Success"
-}
-
-func GetTeamRanking(tx *gorm.DB, contestID uint, limit, offset int) ([]model.Team, int64, bool, string) {
-	var (
-		teams          = make([]model.Team, 0)
-		repo           = db.InitTeamRepo(tx)
-		count, ok, msg = repo.Count(contestID, true, false)
-		err            error
-	)
-	if !ok {
-		return teams, count, false, msg
-	}
-	start, end := utils.TidyPaginate(int(count), limit, offset)
-	if teams, err = redis.GetTeamRanking(contestID, int64(start), int64(end-1)); err == nil && teams != nil {
-		return teams, count, true, "Success"
-	}
-	if ok, msg = UpdateTeamRanking(tx, contestID); err != nil {
-		return teams, count, false, msg
-	}
-	return GetTeamRanking(tx, contestID, limit, offset)
 }
 
 func CalcTeamScore(tx *gorm.DB, teamID uint) (float64, bool, string) {
