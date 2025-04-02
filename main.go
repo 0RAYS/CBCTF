36217d35943ab8b4b74de28cb4e256b4925f9c22
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-02 14:26:50 +0800

    feat: 修改入口

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"CBCTF/internel/config"
-	"CBCTF/internel/log"
-	"CBCTF/internel/redis"
-	"CBCTF/internel/repo"
-	"CBCTF/internel/router"
-	"errors"
-	"fmt"
-	"net/http"
+	"CBCTF/cmd"
 )
 
-func init() {
-	config.Init()
-	log.Init()
-	redis.Init()
-	repo.Init()
-}
-
 func main() {
-	ip, port := config.Env.Gin.Host, config.Env.Gin.Port
-	server := &http.Server{
-		Addr:    fmt.Sprintf("%s:%d", ip, port),
-		Handler: router.Init(),
-	}
-	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		log.Logger.Fatalf("Failed to start: %s", err)
-	}
+	cmd.Cmd()
 }
