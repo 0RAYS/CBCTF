8f500f89bf6636a3498e69a71c5aa787d2bd93f5
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-02 16:08:54 +0800

    fix: 修复无法读取的bug

--- internel/model/public.go
+++ internel/model/public.go
@@ -14,12 +14,12 @@ func (s Strings) Value() (driver.Value, error) {
 	return json.Marshal(s)
 }
 
-func (s Strings) Scan(value interface{}) error {
+func (s *Strings) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Strings value")
 	}
-	return json.Unmarshal(bytes, &s)
+	return json.Unmarshal(bytes, s)
 }
 
 type Uints []uint
@@ -28,12 +28,12 @@ func (u Uints) Value() (driver.Value, error) {
 	return json.Marshal(u)
 }
 
-func (u Uints) Scan(value interface{}) error {
+func (u *Uints) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Uints value")
 	}
-	return json.Unmarshal(bytes, &u)
+	return json.Unmarshal(bytes, u)
 }
 
 type Prize struct {
@@ -47,12 +47,12 @@ func (p Prizes) Value() (driver.Value, error) {
 	return json.Marshal(p)
 }
 
-func (p Prizes) Scan(value interface{}) error {
+func (p *Prizes) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Prizes value")
 	}
-	return json.Unmarshal(bytes, &p)
+	return json.Unmarshal(bytes, p)
 }
 
 type Timeline struct {
@@ -67,12 +67,34 @@ func (t Timelines) Value() (driver.Value, error) {
 	return json.Marshal(t)
 }
 
-func (t Timelines) Scan(value interface{}) error {
+func (t *Timelines) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Timelines value")
 	}
-	return json.Unmarshal(bytes, &t)
+	return json.Unmarshal(bytes, t)
+}
+
+type IPBlock struct {
+	CIDR   string   `json:"cidr"`
+	Except []string `json:"except"`
+}
+
+func isValidIPBlock(ipBlock IPBlock) bool {
+	_, ipNet, err := net.ParseCIDR(ipBlock.CIDR)
+	if err != nil {
+		return false
+	}
+	for _, ex := range ipBlock.Except {
+		_, exNet, err := net.ParseCIDR(ex)
+		if err != nil {
+			return false
+		}
+		if !ipNet.Contains(exNet.IP) {
+			return false
+		}
+	}
+	return true
 }
 
 type NetworkPolicy struct {
@@ -98,12 +120,12 @@ func (n NetworkPolicies) Value() (driver.Value, error) {
 	return json.Marshal(n)
 }
 
-func (n NetworkPolicies) Scan(value interface{}) error {
+func (n *NetworkPolicies) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan NetworkPolicy value")
 	}
-	return json.Unmarshal(bytes, &n)
+	return json.Unmarshal(bytes, n)
 }
 
 // Docker 题目的 Docker 配置, 一个容器可以有多个 flag 和多个映射端口
@@ -119,12 +141,12 @@ func (d Docker) Value() (driver.Value, error) {
 	return json.Marshal(d)
 }
 
-func (d Docker) Scan(value interface{}) error {
+func (d *Docker) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Docker value")
 	}
-	return json.Unmarshal(bytes, &d)
+	return json.Unmarshal(bytes, d)
 }
 
 type Dockers []Docker
@@ -133,12 +155,12 @@ func (d Dockers) Value() (driver.Value, error) {
 	return json.Marshal(d)
 }
 
-func (d Dockers) Scan(value interface{}) error {
+func (d *Dockers) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Dockers value")
 	}
-	return json.Unmarshal(bytes, &d)
+	return json.Unmarshal(bytes, d)
 }
 
 type Exposes []int32
@@ -147,32 +169,10 @@ func (e Exposes) Value() (driver.Value, error) {
 	return json.Marshal(e)
 }
 
-func (e Exposes) Scan(value interface{}) error {
+func (e *Exposes) Scan(value interface{}) error {
 	bytes, ok := value.([]byte)
 	if !ok {
 		return fmt.Errorf("failed to scan Exposes value")
 	}
-	return json.Unmarshal(bytes, &e)
-}
-
-type IPBlock struct {
-	CIDR   string   `json:"cidr"`
-	Except []string `json:"except"`
-}
-
-func isValidIPBlock(ipBlock IPBlock) bool {
-	_, ipNet, err := net.ParseCIDR(ipBlock.CIDR)
-	if err != nil {
-		return false
-	}
-	for _, ex := range ipBlock.Except {
-		_, exNet, err := net.ParseCIDR(ex)
-		if err != nil {
-			return false
-		}
-		if !ipNet.Contains(exNet.IP) {
-			return false
-		}
-	}
-	return true
+	return json.Unmarshal(bytes, e)
 }
