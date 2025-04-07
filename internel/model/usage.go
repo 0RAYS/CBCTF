57de1b03a541bedd83e0136cdbca96e2b53b0d65
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-07 20:00:10 +0800

    feat: 添加gorm.Model

--- internel/model/usage.go
+++ internel/model/usage.go
@@ -1,21 +1,29 @@
 package model
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type Usage struct {
-	ID          uint         `gorm:"primaryKey" json:"id"`
-	ContestID   uint         `json:"contest_id"`
-	Contest     Contest      `json:"-"`
-	ChallengeID string       `json:"challenge_id"`
-	Challenge   Challenge    `json:"-"`
-	Name        string       `gorm:"not null" json:"name"`
-	Desc        string       `json:"desc"`
-	Hidden      bool         `json:"hidden"`
-	Attempt     int64        `json:"attempt"`
-	Docker      Docker       `gorm:"type:json" json:"docker"`
-	Dockers     Dockers      `gorm:"type:json" json:"dockers"`
-	Hints       Strings      `gorm:"type:json" json:"hints"`
-	Tags        Strings      `gorm:"type:json" json:"tags"`
-	Flags       []Flag       `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
-	Containers  []Container  `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
-	Submissions []Submission `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
-	Version     uint         `gorm:"default:1" json:"-"`
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	ContestID   uint           `json:"contest_id"`
+	Contest     Contest        `json:"-"`
+	ChallengeID string         `json:"challenge_id"`
+	Challenge   Challenge      `json:"-"`
+	Name        string         `gorm:"not null" json:"name"`
+	Desc        string         `json:"desc"`
+	Hidden      bool           `json:"hidden"`
+	Attempt     int64          `json:"attempt"`
+	Docker      Docker         `gorm:"type:json" json:"docker"`
+	Dockers     Dockers        `gorm:"type:json" json:"dockers"`
+	Hints       Strings        `gorm:"type:json" json:"hints"`
+	Tags        Strings        `gorm:"type:json" json:"tags"`
+	Flags       []Flag         `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	Containers  []Container    `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	Submissions []Submission   `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdateAt    time.Time      `json:"-"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
+	Version     uint           `gorm:"default:1" json:"-"`
 }
