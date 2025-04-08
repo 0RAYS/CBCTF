70974d0aac45640efb8a9832281acaf59e747639
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-08 19:30:37 +0800

    fix: 优化更新逻辑

--- internel/cron/flag.go
+++ internel/cron/flag.go
@@ -9,6 +9,9 @@ import (
 	"time"
 )
 
+// UpdateFlagScore 依据数据库, 更新 model.Flag 的分数和解题人数
+// 正常情况下该定时任务无意义, 每次有新解出时即更新 current_score 和 solvers
+// 当 submissions 且 model.Submission.Solved == true 时的数据减少 (例如: 用户注销 / 队伍解散 引发的数据删除), 该函数才有意义
 func UpdateFlagScore(c *cron.Cron) {
 	function := executionTime("UpdateFlagScore", func() {
 		log.Logger.Debug("Update flag score")
@@ -36,15 +39,17 @@ func UpdateFlagScore(c *cron.Cron) {
 						if !ok {
 							return
 						}
-						tx := db.DB.Begin()
-						if ok, _ = flagRepo.Update(flag.ID, db.UpdateFlagOptions{
-							CurrentScore: &currentScore,
-							Solvers:      &solvers,
-						}); !ok {
-							tx.Rollback()
-							return
+						if solvers != flag.Solvers || currentScore != flag.CurrentScore {
+							tx := db.DB.Begin()
+							if ok, _ = flagRepo.Update(flag.ID, db.UpdateFlagOptions{
+								CurrentScore: &currentScore,
+								Solvers:      &solvers,
+							}); !ok {
+								tx.Rollback()
+								return
+							}
+							tx.Commit()
 						}
-						tx.Commit()
 					}()
 					mu.(*sync.Mutex).Unlock()
 				}
