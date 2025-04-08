429d8a25c51aa80d38a06d5b5d534d2829342777
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-08 21:05:55 +0800

    fix:

--- internel/router/usage.go
+++ internel/router/usage.go
@@ -18,17 +18,18 @@ func GetUsageStatus(ctx *gin.Context) {
 	team := middleware.GetTeam(ctx)
 	usage := middleware.GetUsage(ctx)
 	DB := db.DB.WithContext(ctx)
-	var data gin.H
-	data["attempts"] = service.CountAttempts(DB, team, usage)
-	data["init"] = service.IsGenerated(DB, usage, team)
-	data["solved"] = service.IsSolved(DB, team, usage)
-	data["remote"] = service.GetRemoteStatus(DB, usage)
-	data["file"] = func() string {
-		if _, err := os.Stat(usage.Challenge.AttachmentPath(team.ID)); err != nil {
-			return ""
-		}
-		return usage.Challenge.AttachmentPath(team.ID)
-	}()
+	data := gin.H{
+		"attempts": service.CountAttempts(DB, team, usage),
+		"init":     service.IsGenerated(DB, usage, team),
+		"solved":   service.IsSolved(DB, team, usage),
+		"remote":   service.GetRemoteStatus(DB, usage),
+		"file": func() string {
+			if _, err := os.Stat(usage.Challenge.AttachmentPath(team.ID)); err != nil {
+				return ""
+			}
+			return usage.Challenge.AttachmentPath(team.ID)
+		}(),
+	}
 	ctx.JSON(http.StatusOK, gin.H{"msg": "Success", "data": data})
 }
 
