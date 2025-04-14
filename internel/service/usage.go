0e8f940e9a4688c0ce26532a86fc1ad193c49a12
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-14 13:50:07 +0800

    fix:

--- internel/service/usage.go
+++ internel/service/usage.go
@@ -13,7 +13,7 @@ func CreateUsage(tx *gorm.DB, contest model.Contest, form f.CreateUsageForm) ([]
 	usages := make([]model.Usage, 0)
 	failed := make([]string, 0)
 	for _, challengeID := range form.ChallengeIDL {
-		if db.InitUsageRepo(tx).IsUniqueChallenge(contest.ID, challengeID) {
+		if !db.InitUsageRepo(tx).IsUniqueChallenge(contest.ID, challengeID) {
 			continue
 		}
 		// 局部回滚
