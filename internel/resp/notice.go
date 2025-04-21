6b30d5d1967dd5751ff3d28e6c551ffc45125eae
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-04-21 16:01:04 +0800

    fix: 添加返回值

--- internel/resp/notice.go
+++ internel/resp/notice.go
@@ -7,6 +7,7 @@ import (
 
 func GetNoticeResp(notice model.Notice) gin.H {
 	return gin.H{
+		"id":         notice.ID,
 		"title":      notice.Title,
 		"content":    notice.Content,
 		"created_at": notice.CreatedAt,
--- internel/resp/submission.go
+++ internel/resp/submission.go
@@ -7,6 +7,7 @@ import (
 
 func GetSubmissionResp(submission model.Submission) gin.H {
 	return gin.H{
+		"id":           submission.ID,
 		"challenge_id": submission.ChallengeID,
 		"team_id":      submission.TeamID,
 		"user_id":      submission.UserID,
