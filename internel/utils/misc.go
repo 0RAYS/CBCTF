4aeb1a53891a9b12fffdbeb1e9363fa782188b31
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-05-17 14:34:13 +0800

    fix: frpc 客户端命名冲突

--- internel/utils/misc.go
+++ internel/utils/misc.go
@@ -3,8 +3,10 @@ package utils
 import (
 	"github.com/google/uuid"
 	"github.com/vmihailenco/msgpack/v5"
+	"math/rand"
 	"reflect"
 	"strings"
+	"time"
 )
 
 // In 实现 in 判断
@@ -25,6 +27,19 @@ func In(value any, slice any) bool {
 // UUID 生成随机uuid
 func UUID() string {
 	return uuid.New().String()
+}
+
+// RandStr 生成随机字符串
+func RandStr(n int) string {
+	const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+
+	result := make([]byte, n)
+	for i := range result {
+		result[i] = charset[r.Intn(len(charset))]
+	}
+	return string(result)
 }
 
 // UpdateOptions2Map 将Update请求的数据提取出被赋值的结果, 为区分默认的0值和赋值的0值, 表单中的字段都为指针类型
