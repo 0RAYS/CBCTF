ae142dac4c508799aee724b37fe6d1c55ef379e8
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-06-05 21:55:14 +0800

    添加嵌入式结构体

--- internel/model/embed.go
+++ internel/model/embed.go
@@ -5,6 +5,7 @@ import (
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
+	"net"
 	"strings"
 )
 
@@ -56,4 +57,78 @@ func (u *UintList) Scan(value any) error {
 		return fmt.Errorf("failed to scan UintList value")
 	}
 	return json.Unmarshal(bytes, u)
+}
+
+type Target struct {
+	Hostname string   `json:"hostname"`
+	CIDR     string   `json:"cidr"`
+	Except   []string `json:"except"`
+}
+
+func (t Target) isValidIPBlock() bool {
+	if t.Hostname != "" {
+		return true
+	}
+	if t.Hostname == "" && t.CIDR == "" {
+		return false
+	}
+	_, ipNet, err := net.ParseCIDR(t.CIDR)
+	if err != nil {
+		return false
+	}
+	for _, ex := range t.Except {
+		_, exNet, err := net.ParseCIDR(ex)
+		if err != nil {
+			return false
+		}
+		if !ipNet.Contains(exNet.IP) {
+			return false
+		}
+	}
+	return true
+}
+
+type NetworkPolicy struct {
+	From []Target `json:"from"`
+	To   []Target `json:"to"`
+}
+
+var DefaultNetworkPolicy = NetworkPolicy{
+	To: []Target{
+		{
+			CIDR: "0.0.0.0/0",
+			Except: []string{
+				"10.0.0.0/8",
+				"172.16.0.0/12",
+				"192.168.0.0/16",
+				"100.64.0.0/10",
+			},
+		},
+	},
+}
+
+type NetworkPolicies []NetworkPolicy
+
+func (n NetworkPolicies) Value() (driver.Value, error) {
+	for _, p := range n {
+		for i, ipBlock := range p.From {
+			if !ipBlock.isValidIPBlock() {
+				p.From = append(p.From[:i], p.From[i+1:]...)
+			}
+		}
+		for i, ipBlock := range p.To {
+			if !ipBlock.isValidIPBlock() {
+				p.To = append(p.To[:i], p.To[i+1:]...)
+			}
+		}
+	}
+	return json.Marshal(n)
+}
+
+func (n *NetworkPolicies) Scan(value any) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("failed to scan NetworkPolicy value")
+	}
+	return json.Unmarshal(bytes, n)
 }
