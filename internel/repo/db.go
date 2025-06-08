41a018f5bb14b726ce128d9721e856aefaa62845
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-06-08 20:40:00 +0800

    fix

--- internel/repo/db.go
+++ internel/repo/db.go
@@ -65,9 +65,10 @@ func Init() {
 
 	// 指定数据表的存储引擎, 需要支持回滚操作
 	err = DB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(
-		&model.Admin{}, &model.Challenge{}, &model.ChallengeFlag{}, &model.Contest{}, &model.ContestChallenge{},
-		&model.ContestFlag{}, &model.Device{}, &model.Docker{}, &model.DockerGroup{}, &model.Event{}, &model.File{},
-		&model.Notice{}, &model.Request{}, &model.Submission{}, &model.Team{}, &model.TeamFlag{}, &model.User{},
+		&model.Admin{}, &model.Challenge{}, &model.ChallengeFlag{}, &model.Container{}, &model.Contest{},
+		&model.ContestChallenge{}, &model.ContestFlag{}, &model.Device{}, &model.Docker{}, &model.DockerGroup{},
+		&model.Event{}, &model.File{}, &model.Notice{}, &model.Pod{}, &model.Request{}, &model.Submission{},
+		&model.Team{}, &model.TeamFlag{}, &model.User{}, &model.Victim{},
 	)
 	if err != nil {
 		log.Logger.Fatalf("Failed to migrate database: %v", err)
