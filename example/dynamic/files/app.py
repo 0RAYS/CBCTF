0e2e7e38ca3f0d89f9d377917c12deae5bb3f428
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-06-14 22:40:59 +0800

    feat: update  example

--- example/dynamic/files/app.py
+++ example/dynamic/files/app.py
@@ -1,40 +1,26 @@
+import asyncio
 import os
-from base64 import b64decode
-from flask import Flask, request, jsonify, Response
+
+from fastapi import FastAPI, Query, Response
+from fastapi.responses import JSONResponse
 from generator import generate_attachment
+from concurrent.futures import ProcessPoolExecutor
 
-app = Flask(__name__)
+
+app = FastAPI()
 
 
-@app.route('/gen', methods=['GET'])
-def generate():
-    data = request.args.to_dict()
-
-    # 身份验证，限制访问
-    pwd = data.get('pwd')
+@app.get("/gen")
+async def generate(pwd: str = Query(...), id: str = Query(...), flags: str = Query(...)):
     if pwd != os.getenv("generator_pwd"):
-        return jsonify({'error': 'Invalid password'}), 403
+        return JSONResponse(content={'error': 'Invalid password'}, status_code=403)
 
-    id_ = data.get('id')
-    # str: base64(base64(flag1),base64(flag2),...)
-    flags = data.get('flag')
-
-    if not id_ or not flags:
-        return jsonify({'error': 'Invalid parameters'}), 400
     try:
-        # bytes: base64(flag1),base64(flag2),...
-        flags = b64decode(flags)
+        loop = asyncio.get_running_loop()
+        file = await loop.run_in_executor(ProcessPoolExecutor(), generate_attachment, flags.encode())
     except Exception as e:
-        return jsonify({'error': f'Invalid flag encoding: {e}'}), 400
+        return JSONResponse(content={'error': str(e)}, status_code=500)
 
-    # list: [flag1, flag2, ...]
-    # flags = [b64decode(i) for i in flags.split(b",")]
-
-    file = generate_attachment(flags)
-    return Response(file, mimetype='application/zip', headers={
-        'Content-Disposition': f'attachment; filename={id_}.zip'
+    return Response(content=file, media_type="application/zip", headers={
+        'Content-Disposition': f'attachment; filename={id}.zip'
     })
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=False)
--- example/dynamic/files/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# $1: team_id
-# $2: base64(base64(flag1),base64(flag2),...)
-
-# 生成附件
-python3 generator.py $1 $2
