83dd0657f45c9b3ce3489eecde40a5faa8c79cad
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-06-16 14:15:45 +0800

    fix:

--- internel/service/email.go
+++ internel/service/email.go
@@ -3,11 +3,31 @@ package service
 import (
 	f "CBCTF/internel/form"
 	"CBCTF/internel/i18n"
+	"CBCTF/internel/log"
+	"CBCTF/internel/model"
 	"CBCTF/internel/redis"
 	db "CBCTF/internel/repo"
 	"CBCTF/internel/utils"
 	"gorm.io/gorm"
 )
+
+func SendEmail(user model.User) (bool, string) {
+	id := utils.UUID()
+	token, err := utils.Generate(user.ID, user.Name, "email", "email")
+	if err != nil {
+		log.Logger.Warningf("Failed to generate token: %s", err)
+		return false, i18n.UnknownError
+	}
+	ok, msg := redis.SetEmailVerifyToken(user.ID, id)
+	if !ok {
+		return false, msg
+	}
+	if err = utils.SendVerifyEmail(user.Email, token, id); err != nil {
+		log.Logger.Warningf("Failed to send mail: %s", err)
+		return false, i18n.SendEmailError
+	}
+	return true, i18n.Success
+}
 
 func VerifyEmail(tx *gorm.DB, form f.VerifyEmail) (bool, string) {
 	claims, err := utils.Parse(form.Token)
