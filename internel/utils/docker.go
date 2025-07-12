2ccce492137f49e912eb46fa0abf89b0c4ef0d55
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-12 17:13:48 +0800

    fix:

--- internel/utils/docker.go
+++ internel/utils/docker.go
@@ -6,13 +6,19 @@ import (
 	"context"
 	"github.com/compose-spec/compose-go/loader"
 	"github.com/compose-spec/compose-go/types"
+	"gopkg.in/yaml.v3"
 )
 
 func LoadDockerComposeYaml(data string) (*types.Project, bool, string) {
+	var raw map[string]any
+	if err := yaml.Unmarshal([]byte(data), &raw); err != nil {
+		log.Logger.Warningf("Failed to load docker-compose.yml: %v", err)
+		return nil, false, i18n.InvalidDockerComposeYaml
+	}
 	cfg, err := loader.LoadWithContext(context.Background(), types.ConfigDetails{
 		ConfigFiles: []types.ConfigFile{
 			{
-				Content: []byte(data),
+				Config: raw,
 			},
 		},
 	})
