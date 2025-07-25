37085184d2112b12a8379ce8418103c713b4a39e
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-25 22:11:12 +0800

    fix:

--- internel/k8s/k8s.go
+++ internel/k8s/k8s.go
@@ -155,25 +155,24 @@ func updateNodeIPs(ctx context.Context) {
 }
 
 func initExternalNetwork(ctx context.Context) {
-	if _, ok, _ := GetSubnet(ctx, ExternalSubnetName); ok {
-		DeleteSubnet(ctx, ExternalSubnetName)
+	if _, ok, _ := GetSubnet(ctx, ExternalSubnetName); !ok {
+		if _, ok, _ := CreateSubnet(ctx, CreateSubnetOptions{
+			Name:       ExternalSubnetName,
+			CIDR:       config.Env.K8S.ExternalNetwork.CIDR,
+			Gateway:    config.Env.K8S.ExternalNetwork.Gateway,
+			ExcludeIPs: config.Env.K8S.ExternalNetwork.ExcludeIPs,
+			Provider:   fmt.Sprintf("%s.kube-system", ExternalSubnetName),
+		}); !ok {
+			log.Logger.Fatal("Failed to init external network")
+		}
+	} else {
+		log.Logger.Info("External network is already exists")
 	}
-	if _, ok, _ := CreateSubnet(ctx, CreateSubnetOptions{
-		Name:       ExternalSubnetName,
-		CIDR:       config.Env.K8S.ExternalNetwork.CIDR,
-		Gateway:    config.Env.K8S.ExternalNetwork.Gateway,
-		ExcludeIPs: config.Env.K8S.ExternalNetwork.ExcludeIPs,
-		Provider:   fmt.Sprintf("%s.kube-system", ExternalSubnetName),
-	}); !ok {
-		log.Logger.Fatal("Failed to init external network")
-	}
-	if _, ok, _ := GetNetAttachDef(ctx, ExternalSubnetName, "kube-system"); ok {
-		DeleteNetAttachDef(ctx, ExternalSubnetName, "kube-system")
-	}
-	if _, ok, _ := CreateNetAttachDef(ctx, CreateNetAttachDefOptions{
-		Name:      ExternalSubnetName,
-		Namespace: "kube-system",
-		Config: fmt.Sprintf(`{
+	if _, ok, _ := GetNetAttachDef(ctx, ExternalSubnetName, "kube-system"); !ok {
+		if _, ok, _ := CreateNetAttachDef(ctx, CreateNetAttachDefOptions{
+			Name:      ExternalSubnetName,
+			Namespace: "kube-system",
+			Config: fmt.Sprintf(`{
 			"cniVersion": "0.3.0",
 			"type": "macvlan",
 			"master": "%s",
@@ -184,31 +183,36 @@ func initExternalNetwork(ctx context.Context) {
 				"provider": "%s.kube-system"
 			}
 		}`, config.Env.K8S.ExternalNetwork.Interface, ExternalSubnetName),
-	}); !ok {
-		log.Logger.Fatal("Failed to init external network attachment definition")
+		}); !ok {
+			log.Logger.Fatal("Failed to init external network attachment definition")
+		}
+	} else {
+		log.Logger.Info("External network is already exists")
 	}
 }
 
 func initNFSVolume(ctx context.Context) {
-	if _, ok, _ := GetPVC(ctx, NFSVolumeName); ok {
-		DeletePVC(ctx, NFSVolumeName)
+	if _, ok, _ := GetPV(ctx, NFSVolumeName); !ok {
+		if _, ok, _ := CreatePV(ctx, CreatePVOptions{
+			Name:    NFSVolumeName,
+			Server:  config.Env.NFS.Server,
+			Path:    config.Env.NFS.Path,
+			Storage: config.Env.NFS.Storage,
+		}); !ok {
+			log.Logger.Fatal("Failed to init pv")
+		}
+	} else {
+		log.Logger.Info("NFS Volume PV is already exists")
 	}
-	if _, ok, _ := GetPV(ctx, NFSVolumeName); ok {
-		DeletePV(ctx, NFSVolumeName)
-	}
-	if _, ok, _ := CreatePV(ctx, CreatePVOptions{
-		Name:    NFSVolumeName,
-		Server:  config.Env.NFS.Server,
-		Path:    config.Env.NFS.Path,
-		Storage: config.Env.NFS.Storage,
-	}); !ok {
-		log.Logger.Fatal("Failed to init pv")
-	}
-	if _, ok, _ := CreatePVC(ctx, CreatePVCOptions{
-		Name:    NFSVolumeName,
-		Storage: config.Env.NFS.Storage,
-	}); !ok {
-		log.Logger.Fatalf("Failed to init pvc")
+	if _, ok, _ := GetPVC(ctx, NFSVolumeName); !ok {
+		if _, ok, _ := CreatePVC(ctx, CreatePVCOptions{
+			Name:    NFSVolumeName,
+			Storage: config.Env.NFS.Storage,
+		}); !ok {
+			log.Logger.Fatalf("Failed to init pvc")
+		}
+	} else {
+		log.Logger.Info("NFS Volume VPC is already exists")
 	}
 	log.Logger.Infof("Please mount the nfs server %s:%s at path %s manually", config.Env.NFS.Server, config.Env.NFS.Path, config.Env.Path)
 }
