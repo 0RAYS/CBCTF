79d20849da3c0bc6a47b7fa954bd3c77d4860bc5
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-25 19:30:49 +0800

    feat: 设置工作目录

--- internel/k8s/generator.go
+++ internel/k8s/generator.go
@@ -81,6 +81,7 @@ func StartGenerator(contestChallenge model.ContestChallenge) (*Generator, bool, 
 						MountPath: "/root/mnt",
 					},
 				},
+				WorkingDir: "/root",
 			},
 		},
 		Volumes: []corev1.Volume{
