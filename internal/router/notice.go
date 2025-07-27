bea47f4be796ceb23dd2b2ac6342517a555dc550
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-27 19:03:47 +0800

    Revert "feat:"
    This reverts commit b06b27b3158e33d5385f2eed26653cad78260da9.

--- internal/router/notice.go
+++ internal/router/notice.go
@@ -9,7 +9,6 @@ import (
 	"CBCTF/internal/service"
 	"CBCTF/internal/websocket"
 	"CBCTF/internal/websocket/model"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -56,7 +55,7 @@ func CreateNotice(ctx *gin.Context) {
 		return
 	}
 	tx.Commit()
-	go websocket.SendToAll(false, model.InfoLevel, model.NoticeType, notice.Title, fmt.Sprintf("%d", notice.ContestID))
+	go websocket.SendToAll(false, model.InfoLevel, model.NoticeType, notice.Title, notice.Content)
 	ctx.JSON(http.StatusOK, gin.H{"msg": i18n.Success, "data": &notice})
 }
 
