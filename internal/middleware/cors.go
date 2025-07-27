5ba5a23847a49d9fe9efbfe77e8099aead817f7c
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-27 16:36:17 +0800

    fix:

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,7 +10,7 @@ import (
 func Cors(ctx *gin.Context) {
 	ctx.Header("Access-Control-Allow-Origin", config.Env.Frontend) // 可将将 * 替换为指定的域名
 	ctx.Header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS")
-	ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-M")
+	ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-M, Connection, Upgrade, Sec-Websocket-Extensions, Sec-Websocket-Key, Sec-Websocket-Version")
 	ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type, Authorization")
 	ctx.Header("Access-Control-Allow-Credentials", "true")
 	if ctx.Request.Method == "OPTIONS" {
