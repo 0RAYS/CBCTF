e94b420531092fb3d45541a42b9fdedb3f374bf2
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-08-01 01:01:50 +0800

    fix:

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -45,11 +45,12 @@ func CheckAuth(ctx *gin.Context) {
 		}
 		magic := GetMagic(ctx)
 		if !utils.CompareMagic(magic, claims.X) {
-			if utils.CompareMagic(model.OauthLoginType, magic) {
+			if utils.CompareMagic(model.OauthLoginType, claims.X) {
 				token, err := utils.GenerateToken(user.ID, user.Name, false, magic)
 				if err != nil {
 					log.Logger.Warningf("Failed to generate token: %s", err)
 					ctx.JSON(http.StatusOK, gin.H{"msg": i18n.UnknownError, "data": nil})
+					return
 				}
 				ctx.Writer.Header().Set("Authorization", fmt.Sprintf("Bearer %s", token))
 			} else {
