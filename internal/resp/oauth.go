bda41e0997bfd125d04df880a8370e435a1bb162
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-31 23:19:08 +0800

    feat:

--- internal/resp/oauth.go
+++ internal/resp/oauth.go
@@ -2,18 +2,10 @@ package resp
 
 import (
 	"CBCTF/internal/model"
-	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOauthResp(oauth model.Oauth) gin.H {
-	runes := []rune(oauth.ClientSecret)
-	var masked string
-	if len(runes) >= 2 {
-		masked = fmt.Sprintf("%s******%s", string(runes[0]), string(runes[len(runes)-1]))
-	} else {
-		masked = fmt.Sprintf("error: too short")
-	}
 	return gin.H{
 		"id":                oauth.ID,
 		"auth_url":          oauth.AuthURL,
@@ -21,7 +13,7 @@ func GetOauthResp(oauth model.Oauth) gin.H {
 		"user_info_url":     oauth.UserInfoURL,
 		"callback_url":      oauth.CallbackURL,
 		"client_id":         oauth.ClientID,
-		"client_secret":     masked,
+		"client_secret":     oauth.ClientSecret,
 		"provider":          oauth.Provider,
 		"uri":               oauth.URI,
 		"resp_id_field":     oauth.RespIDField,
