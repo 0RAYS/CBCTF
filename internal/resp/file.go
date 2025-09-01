b6f243303c52183d31744bbce6e45ed36fca9614
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-09-01 18:29:12 +0800

    feat: 新增字段

--- internal/resp/file.go
+++ internal/resp/file.go
@@ -8,13 +8,18 @@ import (
 
 func GetFileResp(file model.File) gin.H {
 	return gin.H{
-		"id":       file.RandID,
-		"filename": file.Filename,
-		"type":     file.Type,
-		"hash":     file.Hash,
-		"size":     file.Size,
-		"date":     file.CreatedAt,
-		"suffix":   file.Suffix,
-		"uploader": file.UserID.V,
+		"id":           file.RandID,
+		"filename":     file.Filename,
+		"type":         file.Type,
+		"hash":         file.Hash,
+		"size":         file.Size,
+		"date":         file.CreatedAt,
+		"suffix":       file.Suffix,
+		"admin_id":     file.AdminID.V,
+		"user_id":      file.UserID.V,
+		"team_id":      file.TeamID.V,
+		"contest_id":   file.ContestID.V,
+		"oauth_id":     file.OauthID.V,
+		"challenge_id": file.ChallengeID.V,
 	}
 }
