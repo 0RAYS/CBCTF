f835139c058fac96f48475e93504e01028609ec9
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-09-02 18:37:25 +0800

    fix:

--- internal/cron/cheat/device.go
+++ internal/cron/cheat/device.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func CheckSameDevice(contest model.Contest) {
@@ -14,7 +15,11 @@ func CheckSameDevice(contest model.Contest) {
 	if !ok {
 		return
 	}
-	deviceUserMap := make(map[string][]uint)
+	type tmp struct {
+		Time   time.Time
+		UserID uint
+	}
+	deviceUserMap := make(map[string][]tmp)
 	for _, userID := range userIDL {
 		devices, _, ok, _ := db.InitDeviceRepo(db.DB).List(-1, -1, db.GetOptions{
 			Conditions: map[string]any{"user_id": userID},
@@ -23,23 +28,24 @@ func CheckSameDevice(contest model.Contest) {
 			continue
 		}
 		for _, device := range devices {
-			deviceUserMap[device.Magic] = append(deviceUserMap[device.Magic], userID)
+			deviceUserMap[device.Magic] = append(deviceUserMap[device.Magic], tmp{device.CreatedAt, userID})
 		}
 	}
 	repo := db.InitCheatRepo(db.DB)
 	for magic, users := range deviceUserMap {
 		if len(users) > 1 {
 			var str []string
-			for _, userID := range users {
-				str = append(str, strconv.Itoa(int(userID)))
+			for _, user := range users {
+				str = append(str, strconv.Itoa(int(user.UserID)))
 			}
-			for _, userID := range users {
+			for _, user := range users {
 				repo.Create(db.CreateCheatOptions{
-					UserID:    sql.Null[uint]{V: userID, Valid: true},
+					UserID:    sql.Null[uint]{V: user.UserID, Valid: true},
 					ContestID: sql.Null[uint]{V: contest.ID, Valid: true},
 					Magic:     magic,
 					Reason:    fmt.Sprintf(model.SameDeviceMagic, fmt.Sprintf("User %s", strings.Join(str, ", "))),
 					Type:      model.Suspicious,
+					Time:      user.Time,
 				})
 			}
 		}
