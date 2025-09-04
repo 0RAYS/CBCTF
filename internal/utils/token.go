a101e58b6308615fc6d3db7221937dcf67a870ec
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-09-04 17:25:42 +0800

    feat: 简化token解析函数

--- internal/utils/token.go
+++ internal/utils/token.go
@@ -39,21 +39,13 @@ func ParseToken(t string) (*Claims, error) {
 	token, err := jwt.ParseWithClaims(t, &Claims{}, func(token *jwt.Token) (any, error) {
 		return []byte(secret), nil
 	})
-	switch {
-	case errors.Is(err, jwt.ErrTokenMalformed):
-		err = errors.New("that's not even a token")
-	case errors.Is(err, jwt.ErrTokenSignatureInvalid):
-		// Invalid signature
-		err = errors.New("invalid signature")
-	case errors.Is(err, jwt.ErrTokenExpired) || errors.Is(err, jwt.ErrTokenNotValidYet):
-		// Token is either expired or not active yet
-		err = errors.New("invalid signature")
-	case token.Valid:
+	if err != nil {
+		return nil, err
+	}
+	if token.Valid {
 		if claims, ok := token.Claims.(*Claims); ok {
 			return claims, nil
 		}
-	default:
-		err = errors.New("couldn't handle this token")
 	}
 	return nil, errors.New("couldn't handle this token")
 }
