db66bcb8855348faddd683e63c2e8188388027a2
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-09-05 18:18:35 +0800

    feat: 使用系统函数备份文件

--- internal/utils/image.go
+++ internal/utils/image.go
@@ -2,7 +2,7 @@ package utils
 
 import (
 	"fmt"
-	"path/filepath"
+	"os"
 
 	"github.com/disintegration/imaging"
 )
@@ -12,15 +12,14 @@ func ResizeImage(path string, width, height int) error {
 	if err != nil {
 		return err
 	}
+	if err = os.Rename(path, fmt.Sprintf("%s.bak", path)); err != nil {
+		return err
+	}
 	oldWidth, oldHeight := img.Bounds().Dx(), img.Bounds().Dy()
 	if oldWidth < oldHeight {
 		height = int(float64(oldHeight) * (float64(width) / float64(oldWidth)))
 	} else {
 		width = int(float64(oldWidth) * (float64(height) / float64(oldHeight)))
 	}
-	resizedImg := imaging.Resize(img, width, height, imaging.Lanczos)
-	if err = imaging.Save(img, fmt.Sprintf("%s.bak%s", path, filepath.Ext(path))); err != nil {
-		return err
-	}
-	return imaging.Save(resizedImg, path)
+	return imaging.Save(imaging.Resize(img, width, height, imaging.Lanczos), path)
 }
