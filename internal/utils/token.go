d82413295ad9bd72ed446c50c4565f8d620843a2
jbnrz <3537659915@qq.com>
2026-02-14 12:24:06 +0800

    fix: 使用随机值作为 jwt token

--- internal/utils/token.go
+++ internal/utils/token.go
@@ -15,9 +15,7 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
-var JWTSecret = "0RAYS-JBNRZ"
-
-//var JWTSecret = UUID()
+var JWTSecret = UUID()
 
 // GenerateToken 生成token
 func GenerateToken(id uint, name string, isAdmin bool, magic string) (tokenString string, err error) {
