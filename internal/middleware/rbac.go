2a950e45276c039ac4f64c2955acf65ebe1e931e
jbnrz <3537659915@qq.com>
2026-02-22 17:25:17 +0800

    feat:

--- internal/middleware/rbac.go
+++ internal/middleware/rbac.go
@@ -6,12 +6,14 @@ import (
 	"CBCTF/internal/model"
 	"CBCTF/internal/websocket/middleware"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-func RequirePermission(resource, operation string) gin.HandlerFunc {
+func RequirePermission(permission string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		resource, operation := strings.Split(permission, ":")[0], strings.Split(permission, ":")[1]
 		pass, ret := db.InitPermissionRepo(db.DB).CheckUserPermission(middleware.GetSelfID(ctx), resource, operation)
 		if !ret.OK {
 			ctx.AbortWithStatusJSON(http.StatusOK, ret)
