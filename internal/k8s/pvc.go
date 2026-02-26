46cbe9155708bb5e08d2329245724006974be6f1
jbnrz <3537659915@qq.com>
2026-02-26 23:43:34 +0800

    feat: 移除对 K8S pv 的检查

--- internal/k8s/pvc.go
+++ internal/k8s/pvc.go
@@ -8,7 +8,6 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	apierror "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -16,41 +15,6 @@ type CreatePVCOptions struct {
 	Name    string
 	Labels  map[string]string
 	Storage string
-}
-
-func CreatePVC(ctx context.Context, options CreatePVCOptions) (*corev1.PersistentVolumeClaim, model.RetVal) {
-	var (
-		pvc     *corev1.PersistentVolumeClaim
-		storage resource.Quantity
-		err     error
-	)
-	storage, err = resource.ParseQuantity(options.Storage)
-	if err != nil {
-		log.Logger.Warningf("Failed to parse storage resource: %s", err)
-		return nil, model.RetVal{Msg: i18n.Common.UnknownError, Attr: map[string]any{"Error": err.Error()}}
-	}
-	pvc = &corev1.PersistentVolumeClaim{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:   options.Name,
-			Labels: options.Labels,
-		},
-		Spec: corev1.PersistentVolumeClaimSpec{
-			AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteMany},
-			Resources: corev1.VolumeResourceRequirements{
-				Requests: map[corev1.ResourceName]resource.Quantity{
-					corev1.ResourceStorage: storage,
-				},
-			},
-			StorageClassName: new(string),
-			VolumeName:       nfsVolumeName,
-		},
-	}
-	pvc, err = kubeClient.CoreV1().PersistentVolumeClaims(globalNamespace).Create(ctx, pvc, metav1.CreateOptions{})
-	if err != nil {
-		log.Logger.Warningf("Failed to create pvc: %s", err)
-		return nil, model.RetVal{Msg: i18n.K8S.CreateError, Attr: map[string]any{"Model": "PVC", "Error": err.Error()}}
-	}
-	return pvc, model.SuccessRetVal()
 }
 
 func GetPVC(ctx context.Context, name string) (*corev1.PersistentVolumeClaim, model.RetVal) {
