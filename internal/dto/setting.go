b1c8b5ab9f8844c76e472d4e2a16a7f137335725
jbnrz <3537659915@qq.com>
2026-02-27 15:32:25 +0800

    fix: 修正k8s凭证路径绑定策略

--- internal/dto/setting.go
+++ internal/dto/setting.go
@@ -40,7 +40,7 @@ type UpdateSettingForm struct {
 	RedisPort *uint   `form:"redis_port" json:"redis_port" binding:"omitempty,port"`
 	RedisPwd  *string `form:"redis_pwd" json:"redis_pwd" binding:"omitempty,min=1,ascii"`
 
-	K8SConfig       *string `form:"k8s_config" json:"k8s_config" binding:"omitempty,file"`
+	K8SConfig       *string `form:"k8s_config" json:"k8s_config" binding:"omitempty,filepath"`
 	K8SNamespace    *string `form:"k8s_namespace" json:"k8s_namespace" binding:"omitempty,min=1,alphanum"`
 	K8STCPDumpImage *string `form:"k8s_tcpdump" json:"k8s_tcpdump" binding:"omitempty,min=1"`
 
