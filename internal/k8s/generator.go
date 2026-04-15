3082c7832a409e50fe4d70b290a02efa918b0aa8
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2026-04-15 14:33:59 +0800

    使用绝对路径运行生成器命令

--- internal/k8s/generator.go
+++ internal/k8s/generator.go
@@ -121,7 +121,7 @@ func GenAttachment(ctx context.Context, challenge model.Challenge, generator mod
 	flag = base64.StdEncoding.EncodeToString([]byte(strings.TrimSuffix(flag, ",")))
 	filepath := challenge.AttachmentPath(teamID)
 	_ = os.Remove(filepath)
-	command := fmt.Sprintf("./run.sh %d %s", teamID, flag)
+	command := fmt.Sprintf("/root/run.sh %d %s", teamID, flag)
 	log.Logger.Debugf("Executing command in %s: %s", generator.Name, command)
 	if _, _, err = Exec(ctx, generator.Name, pod.Spec.Containers[0].Name, command, nil); err != nil {
 		log.Logger.Warningf("Failed to execute command %s: %s", command, err)
