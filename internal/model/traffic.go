9cf00e2fa25efa17ed46b3211c859077e7c11e41
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2026-06-04 13:21:01 +0800

    fix: 修正索引

--- internal/model/traffic.go
+++ internal/model/traffic.go
@@ -2,7 +2,7 @@ package model
 
 // Traffic 每个 victim 一条，IPs 存储靶机流量中涉及的所有 IP（普通 pod + frpc proxy protocol）。
 type Traffic struct {
-	VictimID uint       `gorm:"uniqueIndex:idx_traffics_victim_id_active,where:deleted_at IS NULL;index" json:"victim_id"`
+	VictimID uint       `gorm:"uniqueIndex;index" json:"victim_id"`
 	IPs      StringList `gorm:"type:jsonb;default:'[]'" json:"ips"`
 	BaseModel
 }
